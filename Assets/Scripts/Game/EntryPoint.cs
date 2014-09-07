27d00b1b16121bbbafe37913c4ef40db98aa1688
artfulchanger <andrea1gentili@gmail.com>
2014-09-07 23:19:59 +0200

    Created Scripts folder, added a single entrypoint

---
+++ Assets/Scripts/Game/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿#region Header
+//  ____              __ 
+// |_  /___ _ _ ___  / / 
+//  / // -_) '_/ _ \/ _ \
+// /___\___|_| \___/\___/
+//
+// Author  : Andrea Gentili, (c) 2014
+// Released under the MIT license
+// -------------------------------------------------------------------------------
+// Main entry point of the application
+#endregion
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Game entry point, everything starts here.
+/// </summary>
+public class EntryPoint : MonoBehaviour {
+
+	/// <summary> 
+	/// Start is called on the frame when the script is enabled, just before any of the Update methods is called the first time.
+	/// </summary> 
+	void Start () {
+	}
+	
+}
